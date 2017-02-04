195aeb4f3ee538d494a9198c166ed574c5af981b
Marcus Lewis <mrcslws@gmail.com>
2017-02-04 13:21:50 -0800

    Actually include js file in the pip package

--- py/setup.py
+++ py/setup.py
@@ -4,12 +4,13 @@ import sys
 from setuptools import find_packages, setup
 
 setup(name="ricotta",
-      version="0.0.2",
+      version="0.0.5",
       description="Visualize bird songs",
       author="Marcus Lewis",
       author_email="mrcslws@gmail.com",
       url="https://github.com/mrcslws/ricotta",
       packages=find_packages(),
-      package_data={'ricotta': ['package_data/*',]},
+      package_data={'ricotta': ['ricotta/package_data/*',]},
       include_package_data=True,
+      zip_safe=False,
 )
